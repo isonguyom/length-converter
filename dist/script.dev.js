76b20eb4377b19866bc52a808bb411c23833d53f
isonguyom <isonguyom@gmail.com>
2022-07-29 14:47:09 +0100

    finish adding  meter conversio formulaen

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -30,6 +30,46 @@ var convert = function convert() {
       //Kilometer
       result = fromValue / 1000;
     }
+
+    if (toUnit == "Mile") {
+      //Mile
+      result = fromValue * 0.000621;
+    }
+
+    if (toUnit == "Inch") {
+      //Inch
+      result = fromValue * 39.370079;
+    }
+
+    if (toUnit == "Foot") {
+      //Foot
+      result = fromValue * 3.28084;
+    }
+
+    if (toUnit == "Yard") {
+      //Yard
+      result = fromValue * 1.093613;
+    }
+
+    if (toUnit == "Rod") {
+      //Rod
+      result = fromValue * 0.1988387815;
+    }
+
+    if (toUnit == "Chain") {
+      //Chain
+      result = fromValue * 0.0497096954;
+    }
+
+    if (toUnit == "Furlong") {
+      //Furlong
+      result = fromValue * 0.0049709695;
+    }
+
+    if (toUnit == "Nautical Mile") {
+      //Nautical Mile
+      result = fromValue * 0.0005399568;
+    }
   } else if (fromUnit == "Kilometer") {
     //Kilometer conversion
     if (toUnit == "Meter") {
